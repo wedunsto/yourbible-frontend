a2de21ae4cb2be96ba9a85c20cefc78e10b9749d
William Dunston <wedunsto@aggies.ncat.edu>
2025-11-27 01:56:33 -0500

    Checked the username for length

--- yourfinance-frontend/src/app/welcome/welcome.page.ts
+++ yourfinance-frontend/src/app/welcome/welcome.page.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   FormBuilder,
   Validators,
   FormsModule,
-  ReactiveFormsModule
+  ReactiveFormsModule,
+  AbstractControl,
+  ValidationErrors
 } from '@angular/forms';
 import { 
   IonContent,
@@ -38,20 +40,56 @@ import { AuthHeaderComponent } from '../components/auth-header/auth-header.compo
     AuthHeaderComponent
   ]
 })
-export class WelcomePage {
+
+export class WelcomePage implements OnInit {
   constructor(
     private fb: FormBuilder,
     private store: Store,
     private router: Router
   ) { }
 
+  ngOnInit() {
+    this.usernameForm.get('username')?.valueChanges.subscribe(() => {
+    this.formSubmitted = false;
+  });
+  }
+
+  formSubmitted: boolean = false;
+
+  // Ensure username has at least 5 characters
+  usernameValidation = (control: AbstractControl) => {
+  const username: string = control.value ?? '';
+  const errors: ValidationErrors = {};
+
+  if (username.length < 5) {
+    errors['minlength'] = true;
+  }
+
+  return Object.keys(errors).length ? errors :null;
+}
+
   usernameForm = this.fb.group({
-    username: ['', [Validators.required]]
+    username: ['', [Validators.required, this.usernameValidation]]
   });
 
+  // Access the username form value
+  get usernameCtrl() {
+    return this.usernameForm.get('username');
+  }
+
+  // Access the username value error object. Check if the length is too short based on the username validation function
+  isUsernameTooShort() {
+    return !!this.usernameCtrl?.hasError('minlength');
+  }
+
+  isUsernameNotModified = () => {
+    return !!(!this.usernameCtrl?.touched && !this.usernameCtrl?.dirty);
+  }
+
   onContinue = () => {
+    this.formSubmitted = true;
     const username = this.usernameForm.get('username')?.value?.trim();
-    if(!username) return;
+    if(!username || this.isUsernameTooShort()) return;
 
     this.store.dispatch(userExistsChecked({ username }));
 
